89a448687337aca61b5368ba9f761bf9094d13c0
broccolai <josh@magmo.co.uk>
2020-05-13 04:41:11 +0100

    Add more message keys

--- src/main/kotlin/co/uk/magmo/puretickets/locale/Messages.kt
+++ src/main/kotlin/co/uk/magmo/puretickets/locale/Messages.kt
@@ -4,7 +4,10 @@ import co.aikar.locales.MessageKey
 import co.aikar.locales.MessageKeyProvider
 
 enum class Messages : MessageKeyProvider {
-    TICKET__CREATED, TICKET__PICKED, TICKET__CLOSED, TICKET__DONE, TITLES__ALL_TICKETS, TITLES__SHOW_TICKET;
+    TASKS__REMINDER,
+    TICKET__CREATED, TICKET__CLOSED, TICKET__PICKED, TICKET__YIELDED, TICKET__DONE, TICKET__REOPENED,
+    NOTIFICATIONS__PICK, NOTIFICATIONS__YIELD, NOTIFICATIONS__DONE, NOTIFICATIONS__REOPEN,
+    TITLES__ALL_TICKETS, TITLES__SHOW_TICKET;
 
     override fun getMessageKey(): MessageKey {
         return MessageKey.of(name.toLowerCase().replace("__", "."))
