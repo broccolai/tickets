438c1533ee6194fd4e43c02cd9645af6a9acc259
broccolai <josh@magmo.co.uk>
2020-06-08 04:41:14 +0100

    Require locations on tickets

--- src/main/kotlin/co/uk/magmo/puretickets/ticket/Ticket.kt
+++ src/main/kotlin/co/uk/magmo/puretickets/ticket/Ticket.kt
@@ -3,7 +3,7 @@ package co.uk.magmo.puretickets.ticket
 import org.bukkit.Location
 import java.util.UUID
 
-data class Ticket(val id: Int, val playerUUID: UUID, val messages: ArrayList<Message>, var status: TicketStatus, var pickerUUID: UUID?, val location: Location?) {
+data class Ticket(val id: Int, val playerUUID: UUID, val messages: ArrayList<Message>, var status: TicketStatus, var pickerUUID: UUID?, val location: Location) {
     fun currentMessage() = messages.lastOrNull { it.reason == MessageReason.MESSAGE }
 
     fun dateOpened() = messages.first { it.reason == MessageReason.MESSAGE }.date
