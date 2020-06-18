1770f3104fbf1028bf059a2aae9f39ba8d5e6255
broccolai <josh@magmo.co.uk>
2020-06-18 18:25:12 +0100

    Change from function

--- src/main/kotlin/co/uk/magmo/puretickets/ticket/TicketStatus.kt
+++ src/main/kotlin/co/uk/magmo/puretickets/ticket/TicketStatus.kt
@@ -6,7 +6,7 @@ enum class TicketStatus(val color: PureColors) {
     OPEN(PureColors.GREEN), PICKED(PureColors.YELLOW), CLOSED(PureColors.RED);
 
     companion object {
-        fun from(input: String): TicketStatus? {
+        fun from(input: String?): TicketStatus? {
             return values().firstOrNull { it.name == input }
         }
     }
