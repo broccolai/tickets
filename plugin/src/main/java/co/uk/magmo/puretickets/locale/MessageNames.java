bcd7f5661dd4834906f4c0242e3fb4fe1403e41d
broccolai <josh@magmo.co.uk>
2020-06-30 00:53:23 +0100

    Convert message names enum to java

--- plugin/src/main/java/co/uk/magmo/puretickets/locale/MessageNames.java
+++ plugin/src/main/java/co/uk/magmo/puretickets/locale/MessageNames.java
@@ -1,8 +1,8 @@
-package co.uk.magmo.puretickets.locale
+package co.uk.magmo.puretickets.locale;
 
-import co.uk.magmo.puretickets.locale.TargetType.*
+import static co.uk.magmo.puretickets.locale.TargetType.*;
 
-enum class MessageNames(vararg val targets: TargetType) {
+public enum MessageNames {
     NEW_TICKET(SENDER, ANNOUNCEMENT, DISCORD),
     UPDATE_TICKET(SENDER, ANNOUNCEMENT, DISCORD),
     CLOSE_TICKET(SENDER, ANNOUNCEMENT, DISCORD),
@@ -12,5 +12,15 @@ enum class MessageNames(vararg val targets: TargetType) {
     DONE_TICKET(SENDER, NOTIFICATION, ANNOUNCEMENT, DISCORD),
     REOPEN_TICKET(SENDER, NOTIFICATION, ANNOUNCEMENT, DISCORD),
     NOTE_TICKET(SENDER, NOTIFICATION, ANNOUNCEMENT, DISCORD),
-    TELEPORT_TICKET(SENDER)
+    TELEPORT_TICKET(SENDER);
+
+    private final TargetType[] targets;
+
+    MessageNames(TargetType... targets) {
+        this.targets = targets;
+    }
+
+    public TargetType[] getTargets() {
+        return targets;
+    }
 }
