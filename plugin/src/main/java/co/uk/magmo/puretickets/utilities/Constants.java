e06983a3490c2345483902af29654be087d97a44
broccolai <josh@magmo.co.uk>
2020-06-30 00:52:12 +0100

    Add contants

---
+++ plugin/src/main/java/co/uk/magmo/puretickets/utilities/Constants.java
@@ -0,0 +1,7 @@
+package co.uk.magmo.puretickets.utilities;
+
+public class Constants {
+    private final String BASE_PERMISSION = "tickets";
+    public final String USER_PERMISSION = BASE_PERMISSION + ".user";
+    public final String STAFF_PERMISSION = BASE_PERMISSION + ".staff";
+}
