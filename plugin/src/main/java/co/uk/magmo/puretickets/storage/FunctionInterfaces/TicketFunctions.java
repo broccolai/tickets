b9fa68a71338204bb5b7aa772198f13037bfe0bf
broccolai <josh@magmo.co.uk>
2020-06-29 04:58:31 +0100

    add ticket functions

--- plugin/src/main/java/co/uk/magmo/puretickets/storage/FunctionInterfaces/TicketFunctions.java
+++ plugin/src/main/java/co/uk/magmo/puretickets/storage/FunctionInterfaces/TicketFunctions.java
@@ -1,37 +1,255 @@
 package co.uk.magmo.puretickets.storage.FunctionInterfaces;
 
-import co.uk.magmo.puretickets.interactions.PendingNotification;
-import co.uk.magmo.puretickets.ticket.Message;
+import co.aikar.idb.DB;
+import co.aikar.idb.DbRow;
+import co.uk.magmo.puretickets.storage.TimeAmount;
 import co.uk.magmo.puretickets.ticket.Ticket;
 import co.uk.magmo.puretickets.ticket.TicketStatus;
-import com.google.common.collect.ArrayListMultimap;
+import co.uk.magmo.puretickets.utilities.ListUtilities;
+import co.uk.magmo.puretickets.utilities.UserUtilities;
 import org.bukkit.Location;
+import org.intellij.lang.annotations.Language;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
 
-public interface TicketFunctions {
-    Ticket select(Integer id);
+public class TicketFunctions {
+    private final HelpersSQL helpers;
 
-    List<Ticket> selectAll(TicketStatus status);
+    public TicketFunctions(HelpersSQL helpers) {
+        this.helpers = helpers;
+    }
 
-    List<Ticket> selectAll(UUID uuid, TicketStatus status);
+    public Ticket select(Integer id) {
+        DbRow row;
 
-    List<Integer> selectIds(UUID uuid, TicketStatus status);
+        try {
+            row = DB.getFirstRow("SELECT id, uuid, status, picker, location from puretickets_ticket WHERE id = ?", id);
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
 
-    Integer selectHighestId(UUID uuid, TicketStatus... statuses);
+        return helpers.buildTicket(row);
+    }
 
-    List<String> selectNames(TicketStatus status);
+    public List<Ticket> selectAll(TicketStatus status) {
+        List<DbRow> results;
 
-    HashMap<TicketStatus, Integer> selectTicketStats(UUID uuid);
+        try {
+            if (status == null) {
+                results = DB.getResults("SELECT id, uuid, status, picker, location FROM puretickets_ticket");
+            } else {
+                results = DB.getResults("SELECT id, uuid, status, picker, location FROM puretickets_ticket WHERE status = ?", status.name());
+            }
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
 
-    Boolean exists(Integer id);
+        return ListUtilities.map(results, helpers::buildTicket);
+    }
 
-    Integer count(TicketStatus status);
+    public List<Ticket> selectAll(UUID uuid, TicketStatus status) {
+        List<DbRow> results;
 
-    Integer insert(UUID uuid, TicketStatus status, UUID picker, Location location);
+        @Language("SQL")
+        String sql = "SELECT id, uuid, status, picker, location from puretickets_ticket WHERE uuid = ?";
 
-    void update(Ticket ticket);
+        try {
+            if (status == null) {
+                results = DB.getResults(sql, uuid.toString());
+            } else {
+                results = DB.getResults(sql + " AND status = ?", uuid.toString(), status.name());
+            }
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+
+        return ListUtilities.map(results, helpers::buildTicket);
+    }
+
+    public List<Integer> selectIds(UUID uuid, TicketStatus status) {
+        List<Integer> results;
+
+        @Language("SQL")
+        String sql = "SELECT id from puretickets_ticket WHERE uuid = ?";
+
+        try {
+            if (status == null) {
+                results = DB.getFirstColumnResults(sql, uuid.toString());
+            } else {
+                results = DB.getFirstColumnResults(sql + " AND status = ?", uuid.toString(), status.name());
+            }
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+
+        return results;
+    }
+
+    public Integer selectHighestId(UUID uuid, TicketStatus... statuses) {
+        ArrayList<String> replacements = new ArrayList<>();
+
+        @Language("SQL")
+        String sql = "SELECT max(id) FROM puretickets_ticket WHERE uuid = ?";
+
+        for (int i = 0; i < statuses.length; i++) {
+            if (i == 0) {
+                sql += " AND status = ?";
+            } else {
+                sql += " OR status = ?";
+            }
+
+            replacements.add(statuses[i].name());
+        }
+
+        try {
+            return DB.getFirstColumn(sql, uuid.toString(), replacements);
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<String> selectNames(TicketStatus status) {
+        List<String> results;
+
+        @Language("SQL")
+        String sql = "SELECT uuid from puretickets_ticket";
+
+        try {
+            if (status == null) {
+                results = DB.getFirstColumnResults(sql);
+            } else {
+                results = DB.getFirstColumnResults(sql + " WHERE status = ?", status.name());
+            }
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+
+        return ListUtilities.map(results, result -> {
+            UUID uuid = UUID.fromString(result);
+            return UserUtilities.nameFromUUID(uuid);
+        });
+    }
+
+    public EnumMap<TicketStatus, Integer> selectTicketStats(UUID uuid) {
+        DbRow row;
+
+        @Language("SQL")
+        String sql = "SELECT " +
+                "SUM(Status LIKE 'OPEN') AS open, " +
+                "SUM(Status LIKE 'PICKED') AS picked, " +
+                "SUM(status LIKE 'CLOSED') AS closed " +
+                "from puretickets_ticket ";
+
+        try {
+            if (uuid == null) {
+                row = DB.getFirstRow(sql);
+            } else {
+                row = DB.getFirstRow(sql + " WHERE uuid = ?", uuid.toString());
+            }
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+
+        EnumMap<TicketStatus, Integer> results = new EnumMap<>(TicketStatus.class);
+
+        results.put(TicketStatus.OPEN, row.getInt("open"));
+        results.put(TicketStatus.PICKED, row.getInt("picked"));
+        results.put(TicketStatus.CLOSED, row.getInt("closed"));
+
+        return results;
+    }
+
+    public Boolean exists(Integer id) {
+        try {
+            Integer value = DB.getFirstColumn("SELECT EXISTS(SELECT 1 from puretickets_ticket WHERE id = ?)", id);
+
+            return value == 1;
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Integer count(TicketStatus status) {
+        @Language("SQL")
+        String sql = "SELECT COUNT(id) FROM puretickets_ticket";
+
+        try {
+            if (status == null) {
+                return DB.getFirstColumn(sql);
+            } else {
+                return DB.getFirstColumn(sql + " WHERE status = ?", status.name());
+            }
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Integer insert(UUID uuid, TicketStatus status, UUID picker, Location location) {
+        Integer index;
+
+        try {
+            index = DB.getFirstColumn("SELECT max(id) from puretickets_ticket");
+
+            if (index == null) {
+                index = 1;
+            } else {
+                index += 1;
+            }
+
+            DB.executeInsert("INSERT INTO puretickets_ticket(id, uuid, status, picker, location) VALUES(?, ?, ?, ?, ?)",
+                    index, uuid.toString(), status.name(), picker, helpers.serializeLocation(location));
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+
+        return index;
+    }
+
+    public void update(Ticket ticket) {
+        UUID pickerUUID = ticket.getPickerUUID();
+        String picker;
+
+        if (pickerUUID == null) {
+            picker = null;
+        } else {
+            picker = pickerUUID.toString();
+        }
+
+        DB.executeUpdateAsync("UPDATE puretickets_ticket SET status = ?, picker = ? WHERE id = ?",
+                ticket.getStatus().name(), ticket.getPickerUUID().toString(), ticket.getId());
+    }
+
+    public HashMap<UUID, Integer> highscores(TimeAmount span) {
+        HashMap<UUID, Integer> data = new HashMap<>();
+        long length;
+
+        if (span.getLength() == null) {
+            length = 0;
+        } else {
+            length = LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond() - span.getLength();
+        }
+
+        @Language("SQL")
+        String sql = "SELECT picker, COUNT(*) AS `num` " +
+                "FROM ticket " +
+                "WHERE status = ? " +
+                "AND picker IS NOT NULL " +
+                "and id in (SELECT DISTINCT ticket FROM message WHERE date > ?) " +
+                "GROUP BY picker";
+
+        List<DbRow> results;
+
+        try {
+            results = DB.getResults(sql, TicketStatus.CLOSED.name(), length);
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+
+        results.forEach(result -> data.put(helpers.getUUID(result, "picker"), result.getInt("num")));
+
+        return data;
+    }
 }
