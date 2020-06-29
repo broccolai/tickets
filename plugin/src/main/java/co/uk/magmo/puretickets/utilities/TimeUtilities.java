f6606adc6185938ff36738cc996d6f4d03689536
broccolai <josh@magmo.co.uk>
2020-06-30 00:39:46 +0100

    Add time utilities

---
+++ plugin/src/main/java/co/uk/magmo/puretickets/utilities/TimeUtilities.java
@@ -0,0 +1,14 @@
+package co.uk.magmo.puretickets.utilities;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeUtilities {
+    public static String formatted(LocalDateTime time) {
+        return time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public static Long minuteToLong(Integer minute) {
+        return (long) (minute * 60 * 20);
+    }
+}
