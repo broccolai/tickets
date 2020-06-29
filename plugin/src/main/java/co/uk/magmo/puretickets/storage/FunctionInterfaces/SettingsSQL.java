4498142e4c00375d9146ae4eba2ff26dd9b2867c
broccolai <josh@magmo.co.uk>
2020-06-29 04:56:14 +0100

    Add settings functions

---
+++ plugin/src/main/java/co/uk/magmo/puretickets/storage/FunctionInterfaces/SettingsSQL.java
@@ -0,0 +1,56 @@
+package co.uk.magmo.puretickets.storage.FunctionInterfaces;
+
+import co.aikar.idb.DB;
+import co.aikar.idb.DbRow;
+import co.uk.magmo.puretickets.user.UserSettings;
+import org.bukkit.Bukkit;
+
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class SettingsSQL {
+    public UserSettings select(UUID uuid) {
+        DbRow result;
+
+        try {
+            result = DB.getFirstRow("SELECT announcements from puretickets_settings WHERE uuid = ?",
+                    uuid.toString());
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+
+        boolean announcements = result.getString("announcements").equals("1");
+
+        return new UserSettings(announcements);
+    }
+
+    public Boolean exists(UUID uuid) {
+        try {
+            Integer value = DB.getFirstColumn("SELECT EXISTS(SELECT 1 from puretickets_settings WHERE uuid = ?)",
+                    uuid.toString());
+
+            return value == 1;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public void insert(UUID uuid, UserSettings settings) {
+        try {
+            DB.executeInsert("INSERT INTO puretickets_settings(uuid, announcements) VALUES(?, ?)",
+                    uuid.toString(), settings.getAnnouncements());
+        } catch (SQLException e) {
+            Bukkit.getLogger().warning("Failed to insert user settings " + uuid.toString());
+        }
+    }
+
+    public void update(UUID uuid, UserSettings settings) {
+        try {
+            DB.executeUpdate("UPDATE puretickets_settings SET announcements = ? WHERE uuid = ?",
+                    settings.getAnnouncements(), uuid.toString());
+        } catch (SQLException e) {
+            Bukkit.getLogger().warning("Failed to update user settings " + uuid.toString());
+        }
+    }
+}
+
