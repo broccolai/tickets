58ccccefa27aa4fe35236b88ef38747936355748
broccolai <josh@magmo.co.uk>
2020-06-30 02:10:05 +0100

    cleanup constants

--- plugin/src/main/java/co/uk/magmo/puretickets/utilities/Constants.java
+++ plugin/src/main/java/co/uk/magmo/puretickets/utilities/Constants.java
@@ -1,7 +1,8 @@
 package co.uk.magmo.puretickets.utilities;
 
 public class Constants {
-    private final String BASE_PERMISSION = "tickets";
-    public final String USER_PERMISSION = BASE_PERMISSION + ".user";
-    public final String STAFF_PERMISSION = BASE_PERMISSION + ".staff";
+    private static final String BASE_PERMISSION = "tickets";
+
+    public static final String USER_PERMISSION = BASE_PERMISSION + ".user";
+    public static final String STAFF_PERMISSION = BASE_PERMISSION + ".staff";
 }
