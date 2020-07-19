af51818f9e61b0e699e47d707cb15cdd40f6fa2c
broccolai <josh@magmo.co.uk>
2020-07-19 17:05:43 +0100

    Add authorization headers

--- plugin/src/main/java/co/uk/magmo/puretickets/integrations/DiscordManager.java
+++ plugin/src/main/java/co/uk/magmo/puretickets/integrations/DiscordManager.java
@@ -1,34 +1,40 @@
 package co.uk.magmo.puretickets.integrations;
 
 import co.uk.magmo.puretickets.configuration.Config;
-import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.intellectualsites.http.EntityMapper;
 import com.intellectualsites.http.HttpClient;
 import com.intellectualsites.http.external.GsonMapper;
 
+import java.util.Base64;
 import java.util.HashMap;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 public class DiscordManager {
-    HttpClient client = HttpClient.newBuilder()
-            .withBaseURL("https://tickets.magmo.co.uk/api/v2")
-            .build();
+    private final HttpClient client;
 
     private final Logger logger;
-
     private final Boolean enabled;
-    private final String guild;
-    private final String token;
 
     public DiscordManager(Logger logger, Config config) {
+        this.client = HttpClient.newBuilder()
+                .withBaseURL("http://localhost:400/api/v1")
+                .withDecorator((req) -> {
+                    String raw = config.DISCORD__GUILD + ":" + config.DISCORD__TOKEN;
+                    byte[] encoded = Base64.getEncoder().encode(raw.getBytes());
+                    String header = "Basic " + new String(encoded);
+
+                    req.withHeader("Authorization", header);
+                })
+                .build();
+
         this.logger = logger;
         this.enabled = config.DISCORD__ENABLED;
-        this.guild = config.DISCORD__GUILD;
-        this.token = config.DISCORD__TOKEN;
     }
 
-    public void sendInformation(String color, String author, Integer id, String action, HashMap<String, String> fields) {
+    public void sendInformation(String color, String author, UUID uuid, Integer id, String action, HashMap<String, String> fields) {
         if (!enabled) return;
 
         JsonObject json = new JsonObject();
@@ -36,6 +42,7 @@ public class DiscordManager {
         json.addProperty("color", color);
         json.addProperty("author", author);
         json.addProperty("id", id);
+        json.addProperty("uuid", uuid.toString());
         json.addProperty("action", action);
         json.addProperty("color", color);
 
@@ -48,12 +55,10 @@ public class DiscordManager {
         }
 
         EntityMapper entityMapper = EntityMapper.newInstance()
-                .registerSerializer(JsonObject.class, GsonMapper.serializer(JsonObject.class, new Gson()));
-
+                .registerSerializer(JsonObject.class, GsonMapper.serializer(JsonObject.class, new GsonBuilder().create()));
         client.post("/announce")
+                .withInput(() -> json)
                 .withMapper(entityMapper)
-                // todo: add actual auth header when backend is rewritten
-                .withHeader(guild, token)
                 .onException(Throwable::printStackTrace)
                 .onStatus(200, response -> {})
                 .onRemaining(response -> logger.warning(String.valueOf(response.getStatusCode())))
