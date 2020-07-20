f1de6052a0b93ef47160e73de146e87d4cbca44c
broccolai <josh@magmo.co.uk>
2020-07-20 16:12:24 +0100

    Update HTTP4J

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     api("org.slf4j:slf4j-simple:1.7.13")
     api("com.zaxxer:HikariCP:2.7.9")
     api("co.uk.magmo:corn-core:1.0.0-SNAPSHOT")
-    api("com.intellectualsites.http:HTTP4J:1.0-SNAPSHOT")
+    api("com.intellectualsites.http:HTTP4J:1.3-SNAPSHOT")
 
     compileOnly("org.spigotmc:spigot-api:1.16.1-R0.1-SNAPSHOT")
 }
