2d9f8039447f10d8b9851b396062d2bb1e583fdd
broccolai <josh@magmo.co.uk>
2020-08-18 03:01:58 +0100

    Remove legacy

--- discord/src/lib/app.ts
+++ discord/src/lib/app.ts
@@ -3,15 +3,12 @@ import express from 'express';
 
 import auth from './providers/auth';
 import announce from './routes/announce';
-import legacy from './routes/legacy';
 import status from './routes/status';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.use('/', legacy);
 
 const api = '/api/v1';
 
