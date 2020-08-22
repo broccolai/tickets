b07f3a8a17f569a8a57755fc5dda4d7992a69655
broccolai <josh@magmo.co.uk>
2020-08-22 01:20:27 +0100

    Add remaining actions

--- discord/src/api/v2/constructs/Action.ts
+++ discord/src/api/v2/constructs/Action.ts
@@ -1,7 +1,15 @@
 const Action: {
   [key: string]: { title: string; author: string };
 } = {
-  PICKED: { title: 'Ticket Picked', author: 'Picked by ' },
+  NEW_TICKET: { title: 'Ticket Created', author: 'Created by ' },
+  UPDATE_TICKET: { title: 'Ticket Updated', author: 'Updated by ' },
+  CLOSE_TICKET: { title: 'Ticket Closed', author: 'Closed by ' },
+  PICK_TICKET: { title: 'Ticket Picked', author: 'Picked by ' },
+  YIELD_TICKET: { title: 'Ticket Yielded', author: 'Yielded by ' },
+  ASSIGN_TICKET: { title: 'Ticket Assigned', author: 'Assigned by ' },
+  DONE_TICKET: { title: 'Ticket Done-marked', author: 'Done-marked by ' },
+  REOPEN_TICKET: { title: 'Ticket Reopened', author: 'Reopened by ' },
+  NOTE_TICKET: { title: 'Note added to Ticket', author: 'Added by ' },
 };
 
 export default Action;
