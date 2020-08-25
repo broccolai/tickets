cce7fdd588460a62bf7b3c79d4e6a9125a2e9ea6
broccolai <josh@magmo.co.uk>
2020-08-25 17:16:22 +0100

    Add serialiser and deserialiser for Location

--- backend/src/constructs/Location.ts
+++ backend/src/constructs/Location.ts
@@ -5,4 +5,19 @@ type Location = {
   z: number;
 };
 
+export const serialiseLocation = ({ world, x, y, z }: Location): string => {
+  return world + '|' + x + '|' + y + '|' + z;
+};
+
+export const deserialiseLocation = (data: string): Location => {
+  const split = data.split('|');
+
+  return {
+    world: split[0],
+    x: Number(split[1]),
+    y: Number(split[2]),
+    z: Number(split[3]),
+  };
+};
+
 export default Location;
