0d02733f46b2902c6f4ab58b14f2a97e4b696a25
broccolai <josh@magmo.co.uk>
2020-08-26 02:00:18 +0100

    Rewrite announce.ts, insert tickets

--- backend/src/api/v2/routes/announce.ts
+++ backend/src/api/v2/routes/announce.ts
@@ -9,60 +9,43 @@ import { wrap } from '@lib/utilities/strings';
 import Action from '@constructs/Action';
 import MessageData from '@constructs/MessageData';
 import TicketStatus from '@constructs/TicketStatus';
-import Ticket from '@constructs/Ticket';
 import db from '@lib/providers/database';
 import { serialiseLocation } from '@constructs/Location';
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-  const authReq = req as IBasicAuthedRequest;
-  const guild = servers.get(authReq.auth.user);
-
   res.sendStatus(200);
 
-  const data = req.body as MessageData;
-  const channel = (await client.channels.fetch(guild.output)) as TextChannel;
-  let ticket: Ticket;
+  const authReq = req as IBasicAuthedRequest;
+  const { output } = servers.get(authReq.auth.user);
+  const { ticket, author, action } = req.body as MessageData;
+  const { id, player, location, status, message } = ticket;
+  const channel = (await client.channels.fetch(output)) as TextChannel;
 
-  channel.send((mb) =>
+  await channel.send((mb) =>
     mb.setEmbed((embed: Embed) => {
-      ticket = data.ticket;
-      const location = ticket.location;
-
       return embed
-        .setColor(TicketStatus[ticket.status])
-        .setAuthor(ticket.player.name, 'https://crafatar.com/avatars/' + ticket.player.uuid)
-        .setTitle('' + Action[data.action].title + ' - #' + ticket.id + '')
+        .setColor(TicketStatus[status])
+        .setAuthor(player.name, 'https://crafatar.com/avatars/' + player.uuid)
+        .setTitle(Action[action].title + ' - #' + id)
         .setDescription(
           `\`\`\`YAML
-UUID: ${ticket.player.uuid}
+UUID: ${player.uuid}
 World: ${location.world}
 Location: X: ${location.x}, Y: ${location.y}, Z: ${location.z}\`\`\``,
         )
-        .addField('MESSAGE', wrap(data.ticket.message, 65))
+        .addField('MESSAGE', wrap(message, 65))
         .setTimestamp(Date.now())
-        .setFooter(Action[data.action].author + data.author.name, 'https://crafatar.com/avatars/' + data.author.uuid);
+        .setFooter(Action[action].author + author.name, 'https://crafatar.com/avatars/' + author.uuid);
     }),
   );
 
-  db.run('INSERT INTO player(uuid, name) values(?, ?) ON CONFLICT(uuid) DO UPDATE set name = ?', [
-    ticket.player.uuid,
-    ticket.player.name,
-    ticket.player.name,
-  ]);
+  db.run('INSERT INTO player(uuid, name) values(?, ?) ON CONFLICT(uuid) DO UPDATE set name = ?', [player.uuid, player.name, player.name]);
 
   db.run(
-    'INSERT INTO ticket(id, player, location, status, message) values(?, ?, ?, ?, ?) ON CONFLICT(id) DO UPDATE set status = ?, message = ?',
-    [
-      ticket.id,
-      ticket.player,
-      serialiseLocation(ticket.location),
-      ticket.status,
-      ticket.message,
-      ticket.status,
-      ticket.message,
-    ],
+    'INSERT INTO ticket(guild, id, player, location, status, message) values(?, ?, ?, ?, ?, ?) ON CONFLICT(guild, id) DO UPDATE set status = ?, message = ?',
+    [authReq.auth.user, id, player.uuid, serialiseLocation(location), status, message, status, message],
   );
 });
 
