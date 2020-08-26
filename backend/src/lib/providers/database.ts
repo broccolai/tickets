0111557a287f088380197347b30479558340a412
broccolai <josh@magmo.co.uk>
2020-08-26 01:59:40 +0100

    Add primary keys to tables

--- backend/src/lib/providers/database.ts
+++ backend/src/lib/providers/database.ts
@@ -5,8 +5,10 @@ import PureGuild from '../constructs/PureGuild';
 const db = new sqlite3.Database('./storage.db', sqlite3.OPEN_READWRITE);
 
 db.run('CREATE TABLE IF NOT EXISTS server(guild TEXT, token TEXT, outputChannel TEXT)');
-db.run('CREATE TABLE IF NOT EXISTS ticket(id NUMERIC, player UUID, location TEXT, status TEXT, message TEXT');
-db.run('CREATE TABLE IF NOT EXIST player(uuid TEXT, name TEXT)');
+db.run(
+  'CREATE TABLE IF NOT EXISTS ticket(guild TEXT, id INTEGER, player UUID, location TEXT, status TEXT, message TEXT, PRIMARY KEY(guild, id))',
+);
+db.run('CREATE TABLE IF NOT EXISTS player(uuid TEXT PRIMARY KEY, name TEXT)');
 
 export const getData = (): Map<string, PureGuild> => {
   const servers = new Map<string, PureGuild>();
