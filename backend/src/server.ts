f58e286b8071237cd6c15325f8664c3254f1af2f
broccolai <me@broccol.ai>
2020-08-29 02:24:23 +0100

    Add logs to stages of backend startup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config.js';
 import 'module-alias/register';
-import app from './lib/app';
-import client from './lib/client';
+import app from '@lib/app';
+import client from '@lib/client';
 
 client.token = process.env.DISCORD_ACCESS_TOKEN;
-client.connect();
+client
+  .connect()
+  .then(() => console.info('PureTickets connected to Discord with token'))
+  .catch(() => console.error('PureTickets backend could not connect to Discord, check your token'));
 
-app.listen(process.env.PORT, () => console.log(`App listening!`));
+const port = process.env.PORT || 400;
+app.listen(port, () => console.info(`PureTickets backend running on port ${port}`));
