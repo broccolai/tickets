32234c8f46cc96f1ee22fff40882299feb80615b
broccolai <josh@magmo.co.uk>
2020-08-29 12:15:53 +0100

    lint

--- backend/src/lib/utilities/strings.ts
+++ backend/src/lib/utilities/strings.ts
@@ -1,2 +1,1 @@
-export const wrap = (s: string, w: number): string =>
-  s.replace(new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, 'g'), '$1\n');
+export const wrap = (s: string, w: number): string => s.replace(new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, 'g'), '$1\n');
