77db1d17ddbbd2acbe1700be143ee4c278577972
broccolai <me@broccol.ai>
2020-08-31 03:30:26 +0100

    Document AutoStatuses

--- plugin/src/main/java/broccolai/tickets/commands/AutoStatuses.java
+++ plugin/src/main/java/broccolai/tickets/commands/AutoStatuses.java
@@ -4,9 +4,18 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * An annotation to represent that statuses a argument could use.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
 public @interface AutoStatuses {
+    /**
+     * Get the string value.
+     * @return the ticket statuses, separated by commas
+     */
+    @NotNull
     String value();
 }
