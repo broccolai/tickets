126433c444d07a3de3abadaf22b48ce385414c9e
broccolai <me@broccol.ai>
2020-12-11 21:09:56 +0000

    Remove dep on corn-spigot for corn-core

--- plugin/core/build.gradle.kts
+++ plugin/core/build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     api("com.github.ben-manes.caffeine:caffeine:2.8.6")
 
     // Corn
-    api("broccolai:corn-spigot:1.1.1")
+    api("broccolai:corn-core:1.1.1")
 
     // Command library
     api("cloud.commandframework:cloud-core:1.2.0")
