40e204e4b91f46321783c9806911e538fd25ebe1
broccolai <me@broccol.ai>
2020-12-11 21:10:15 +0000

    Fix shading and relocations

--- plugin/bukkit/build.gradle.kts
+++ plugin/bukkit/build.gradle.kts
@@ -22,6 +22,38 @@ tasks {
     }
 
     shadowJar {
+        fun relocates(vararg dependencies: String) {
+            dependencies.forEach {
+                val split = it.split('.')
+                val name = split.last();
+                relocate(it, "${rootProject.group}.dependencies.$name")
+            }
+        }
+
+        dependencies {
+            exclude(dependency("com.google.guava:"))
+            exclude(dependency("com.google.errorprone:"))
+            exclude(dependency("org.checkerframework:"))
+            exclude(dependency("org.jetbrains:"))
+            exclude(dependency("org.intellij:"))
+        }
+
+        relocates(
+                "com.intellectualsites.http",
+                "com.github.benmanes.caffeine",
+                "org.antlr",
+                "org.objectweb",
+                "org.slf4j",
+                "org.jdbi",
+                "io.leangen.geantyref",
+                "io.papermc.lib",
+                "cloud.commandframework",
+                "net.kyori.adventure",
+                "net.kyori.event",
+                "net.kyori.examination",
+                "broccolai.corn"
+        )
+
         archiveClassifier.set(null as String?)
         archiveFileName.set(project.name + ".jar")
         destinationDirectory.set(rootProject.tasks.shadowJar.get().destinationDirectory.get())
