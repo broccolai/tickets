eeadfa4c9dbde98a41bac5aaa1a84e27c7ceda79
broccolai <me@broccol.ai>
2021-02-04 11:32:23 +0000

    Add helper functions to TicketStatus

--- plugin/core/src/main/java/broccolai/tickets/core/ticket/TicketStatus.java
+++ plugin/core/src/main/java/broccolai/tickets/core/ticket/TicketStatus.java
@@ -1,5 +1,6 @@
 package broccolai.tickets.core.ticket;
 
+import cloud.commandframework.arguments.flags.FlagContext;
 import net.kyori.adventure.text.format.TextColor;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
@@ -41,5 +42,27 @@ public enum TicketStatus {
         }
 
         return null;
+    }
+
+    /**
+     * Retrieve an array of statuses from a flag context
+     *
+     * @param flags the flag context to retrieve from
+     * @return array of ticket statuses
+     */
+    public static @NonNull TicketStatus[] fromFlags(final @NonNull FlagContext flags) {
+        TicketStatus status = flags.getValue("status", null);
+
+        return status != null ? of(status) : of(TicketStatus.OPEN, TicketStatus.PICKED);
+    }
+
+    /**
+     * Retrieve an array of statuses from a vararg input
+     *
+     * @param values vararg ticket statuses to generate from
+     * @return array of ticket statuses
+     */
+    public static @NonNull TicketStatus[] of(final @NonNull TicketStatus... values) {
+        return values;
     }
 }
