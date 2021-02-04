8db23c379c5c82f1f17b834886f5dd2e0da99dbd
broccolai <me@broccol.ai>
2021-02-04 11:31:11 +0000

    Initial work on new command structure

--- plugin/core/src/main/java/broccolai/tickets/core/commands/command/BaseCommand.java
+++ plugin/core/src/main/java/broccolai/tickets/core/commands/command/BaseCommand.java
@@ -6,11 +6,6 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 
 public interface BaseCommand {
 
-    /**
-     * Register this command to a command manager
-     *
-     * @param manager the command manager to register to
-     */
     void register(@NonNull CommandManager<UserAudience> manager);
 
 }
