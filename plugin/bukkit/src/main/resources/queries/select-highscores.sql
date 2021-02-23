5b168d9897efc7872bde4a222e844eb83c64360a
broccolai <me@broccol.ai>
2021-02-23 20:44:26 +0000

    Fix highscore command

--- plugin/bukkit/src/main/resources/queries/select-highscores.sql
+++ plugin/bukkit/src/main/resources/queries/select-highscores.sql
@@ -3,5 +3,5 @@ FROM puretickets_ticket
 WHERE status = :status
   AND picker IS NOT NULL
   and id in (SELECT DISTINCT ticket FROM puretickets_message WHERE date
-    > : date)
+    > :date)
 GROUP BY picker
