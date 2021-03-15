451d62fb805eabceb49974d2e747d9b0d2e66bc4
broccolai <me@broccol.ai>
2021-03-15 10:54:51 +0000

    Fix select-tickets sql

--- plugin/bukkit/src/main/resources/queries/select-tickets.sql
+++ plugin/bukkit/src/main/resources/queries/select-tickets.sql
@@ -2,4 +2,4 @@ SELECT t.`id`, t.`player`, t.`position`, t.`status`, t.`claimer`,
        i.`action`, max(i.`time`) AS `time`, i.`sender`, i.`message`
 FROM puretickets_ticket as t
 JOIN puretickets_interaction as i ON t.id = i.ticket
-WHERE puretickets_ticket.id IN (<ids>)
+WHERE t.id IN (<ids>)
