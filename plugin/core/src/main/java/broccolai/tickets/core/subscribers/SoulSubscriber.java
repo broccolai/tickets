68cf453c2aa6e245c3cf9b45e904b338f0b4ffc4
broccolai <me@broccol.ai>
2021-03-19 19:50:23 +0000

    Remind on join

--- plugin/core/src/main/java/broccolai/tickets/core/subscribers/SoulSubscriber.java
+++ plugin/core/src/main/java/broccolai/tickets/core/subscribers/SoulSubscriber.java
@@ -2,22 +2,35 @@ package broccolai.tickets.core.subscribers;
 
 import broccolai.tickets.api.model.event.Subscriber;
 import broccolai.tickets.api.model.event.impl.SoulJoinEvent;
+import broccolai.tickets.api.model.ticket.TicketStatus;
 import broccolai.tickets.api.model.user.PlayerSoul;
 import broccolai.tickets.api.service.event.EventService;
+import broccolai.tickets.api.service.message.MessageService;
 import broccolai.tickets.api.service.storage.StorageService;
+import broccolai.tickets.api.service.ticket.TicketService;
+import broccolai.tickets.core.utilities.Constants;
 import com.google.inject.Inject;
 import net.kyori.adventure.text.Component;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.Collection;
+import java.util.EnumSet;
 
 public final class SoulSubscriber implements Subscriber {
 
     private final StorageService storageService;
+    private final TicketService ticketService;
+    private final MessageService messageService;
 
     @Inject
-    public SoulSubscriber(final @NonNull StorageService storageService) {
+    public SoulSubscriber(
+            final @NonNull StorageService storageService,
+            final @NonNull TicketService ticketService,
+            final @NonNull MessageService messageService
+    ) {
         this.storageService = storageService;
+        this.ticketService = ticketService;
+        this.messageService = messageService;
     }
 
     @Override
@@ -31,6 +44,11 @@ public final class SoulSubscriber implements Subscriber {
 
         for (final Component notification : notifications) {
             soul.sendMessage(notification);
+        }
+
+        if (soul.permission(Constants.STAFF_PERMISSION + ".announce")) {
+            int tickets = this.ticketService.count(EnumSet.of(TicketStatus.OPEN, TicketStatus.CLAIMED));
+            soul.sendMessage(this.messageService.taskReminder(tickets));
         }
     }
 
