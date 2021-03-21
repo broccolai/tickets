9bd9996f097913e911b3f0e41f2b4d075faa2869
broccolai <me@broccol.ai>
2021-03-21 15:24:38 +0000

    Remove unused methods from TicketStatus, NotificationReason and LocaleConfiguration and delete unused class SettingsMapper

--- plugin/api/src/main/java/broccolai/tickets/api/model/ticket/TicketStatus.java
+++ plugin/api/src/main/java/broccolai/tickets/api/model/ticket/TicketStatus.java
@@ -3,14 +3,10 @@ package broccolai.tickets.api.model.ticket;
 import cloud.commandframework.arguments.flags.FlagContext;
 import net.kyori.adventure.text.format.TextColor;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.EnumSet;
 import java.util.Set;
 
-/**
- * enum representing the potential states of a ticket
- */
 public enum TicketStatus {
     OPEN(TextColor.color(85, 255, 85)),
     CLAIMED(TextColor.color(255, 255, 85)),
@@ -22,29 +18,10 @@ public enum TicketStatus {
         this.color = color;
     }
 
-    /**
-     * Retrieve the color associated with this status
-     */
     public TextColor color() {
         return this.color;
     }
 
-    /**
-     * Retrieve a ticket status with a name
-     */
-    public static @Nullable TicketStatus from(final @NonNull String input) {
-        for (TicketStatus value : values()) {
-            if (value.name().equalsIgnoreCase(input)) {
-                return value;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Retrieve an array of statuses from a flag context
-     */
     public static @NonNull Set<TicketStatus> from(final @NonNull FlagContext flags) {
         TicketStatus status = flags.getValue("status", null);
 
