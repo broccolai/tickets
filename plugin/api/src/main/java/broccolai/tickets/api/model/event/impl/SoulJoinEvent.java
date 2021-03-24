dd88f2a0816d92849f20fc8ea0aea14e654d0bfb
broccolai <me@broccol.ai>
2021-03-24 22:08:37 +0000

    Add accidental imports

--- plugin/api/src/main/java/broccolai/tickets/api/model/event/impl/SoulJoinEvent.java
+++ plugin/api/src/main/java/broccolai/tickets/api/model/event/impl/SoulJoinEvent.java
@@ -3,7 +3,6 @@ package broccolai.tickets.api.model.event.impl;
 import broccolai.tickets.api.model.event.SoulEvent;
 import broccolai.tickets.api.model.user.PlayerSoul;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.jetbrains.annotations.NotNull;
 
 public final class SoulJoinEvent implements SoulEvent {
 
