28e566c25fe0bc188d95e389aa70781f2b5ae37e
broccolai <me@broccol.ai>
2021-03-25 04:56:29 +0000

    Rename other usages of Sponge that should be Sponge7

--- plugin/settings.gradle.kts
+++ plugin/settings.gradle.kts
@@ -1,11 +1,11 @@
 rootProject.name = "tickets"
 
-include("api", "core", "bukkit", "sponge")
+include("api", "core", "bukkit", "sponge7")
 
 project(":api").name = "tickets-api"
 project(":core").name = "tickets-core"
 project(":bukkit").name = "tickets-bukkit"
-project(":sponge").name = "tickets-sponge"
+project(":sponge7").name = "tickets-sponge7"
 
 enableFeaturePreview("VERSION_CATALOGS")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
