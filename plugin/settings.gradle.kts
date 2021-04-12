67af22f1f8cd54c6ecccb41fa6a1da2a508e5dc5
broccolai <me@broccol.ai>
2021-04-12 20:26:20 +0100

    Use Polyglot Version Catalog

--- plugin/settings.gradle.kts
+++ plugin/settings.gradle.kts
@@ -1,3 +1,14 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.stellardrift.ca/repository/snapshots/")
+    }
+}
+
+plugins {
+    id("ca.stellardrift.polyglot-version-catalogs") version "5.0.0-SNAPSHOT"
+}
+
 rootProject.name = "tickets"
 
 include("api", "core", "bukkit", "sponge7")
