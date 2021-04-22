7101457ea76279318e577a6e9787df2d81e51408
broccolai <me@broccol.ai>
2021-04-22 20:23:44 +0100

    Update to Indra 2.0

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,12 +2,12 @@ import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
 import net.kyori.indra.IndraCheckstylePlugin
 import net.kyori.indra.IndraPlugin
 import net.kyori.indra.IndraPublishingPlugin
-import net.kyori.indra.sonatypeSnapshots
+import net.kyori.indra.repository.sonatypeSnapshots
 
 plugins {
-    id("net.kyori.indra") version "1.3.1"
-    id("net.kyori.indra.publishing") version "1.3.1"
-    id("net.kyori.indra.checkstyle") version "1.3.1"
+    id("net.kyori.indra") version "2.0.0-SNAPSHOT"
+    id("net.kyori.indra.publishing") version "2.0.0-SNAPSHOT"
+    id("net.kyori.indra.checkstyle") version "2.0.0-SNAPSHOT"
     id("com.github.johnrengelman.shadow") version "6.1.0"
     id("com.github.ben-manes.versions") version "0.38.0"
 }
@@ -41,11 +41,11 @@ subprojects {
             publishReleasesTo("broccolai", "https://repo.broccol.ai/releases")
 
             javaVersions {
-                target.set(11)
+                target(11)
             }
 
             github("broccolai", "tickets") {
-                ci = true
+                ci(true)
             }
         }
 
