9c622e3667cea110463b74e3551de38067c6d9f7
broccolai <me@broccol.ai>
2021-05-18 18:54:13 +0100

    Use Paper-API again

--- plugin/bukkit/build.gradle.kts
+++ plugin/bukkit/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     api(projects.ticketsApi)
     api(projects.ticketsCore)
 
-    compileOnly(libs.spigot)
+    compileOnly(libs.paper.api)
     api(libs.paper.lib)
     api(libs.adventure.bukkit)
     api(libs.cloud.paper)
