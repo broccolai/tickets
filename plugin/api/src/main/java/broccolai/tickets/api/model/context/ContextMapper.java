2cfbf290b90ce2d4a14a040b267a1fb1f0ac645d
broccolai <me@broccol.ai>
2021-05-25 22:16:21 +0100

    Fix issues with ContextMapper/ContextService

--- plugin/api/src/main/java/broccolai/tickets/api/model/context/ContextMapper.java
+++ plugin/api/src/main/java/broccolai/tickets/api/model/context/ContextMapper.java
@@ -4,8 +4,8 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 public interface ContextMapper<T> {
 
-    @Nullable String serialize(final @Nullable T value);
+    @Nullable String serialize(@Nullable T value);
 
-    @Nullable T deserialize(final @Nullable T value);
+    @Nullable T deserialize(@Nullable String value);
 
 }
