7ed573ca3fd53ffb79a2ed678895d779b1788724
broccolai <me@broccol.ai>
2021-05-26 14:31:03 +0100

    Remove lingering position column in insert query

--- plugin/bukkit/src/main/resources/queries/insert-ticket.sql
+++ plugin/bukkit/src/main/resources/queries/insert-ticket.sql
@@ -1,5 +1,5 @@
-INSERT INTO puretickets_ticket(`player`, `position`, `status`, `claimer`)
-VALUES (:player, :position, :status, :claimer);
+INSERT INTO puretickets_ticket(`player`, `status`, `claimer`)
+VALUES (:player, :status, :claimer);
 SELECT LAST_INSERT_ID();
 INSERT INTO puretickets_interaction(`ticket`, `action`, `time`, `sender`, `message`)
 VALUES (:ticket, :action, :time, :sender, :message);
