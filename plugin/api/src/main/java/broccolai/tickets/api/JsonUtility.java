e67021a8d9ab55cc39436c565544767e92cbead2
broccolai <me@broccol.ai>
2021-05-29 22:51:58 +0100

    Begin moving over some services to new Ticket data structure

--- plugin/api/src/main/java/broccolai/tickets/api/JsonUtility.java
+++ plugin/api/src/main/java/broccolai/tickets/api/JsonUtility.java
@@ -1,10 +1,13 @@
 package broccolai.tickets.api;
 
+import broccolai.tickets.api.model.interaction.MessageInteraction;
 import broccolai.tickets.api.model.ticket.Ticket;
 import broccolai.tickets.api.model.user.Soul;
 import broccolai.tickets.api.service.user.UserService;
 import com.google.gson.JsonObject;
 import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.Optional;
 
 public final class JsonUtility {
 
@@ -21,7 +24,9 @@ public final class JsonUtility {
         json.add("player", playerJson);
 
         json.addProperty("status", ticket.status().name());
-        json.addProperty("message", ticket.message().message());
+
+        Optional<MessageInteraction> potentialInteraction = ticket.interactions().findLatestMessage(null);
+        potentialInteraction.ifPresent(interaction -> json.addProperty("message", interaction.message()));
 
         return json;
     }
