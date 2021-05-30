6c08c8fce53836dfce6fe5d2e30cded377dea215
broccolai <me@broccol.ai>
2021-05-30 04:59:49 +0100

    Fix sql queries that used a wrong table name

--- plugin/bukkit/src/main/resources/queries/select-tickets-statuses.sql
+++ plugin/bukkit/src/main/resources/queries/select-tickets-statuses.sql
@@ -1,7 +1,7 @@
 SELECT t.id, t.player, t.status, t.claimer,
     i.action, i.time, i.sender, i.message,
     c.namespace, c.name, c.value
-FROM ticket as t
+FROM puretickets_ticket as t
          LEFT JOIN puretickets_interaction as i
                    ON (t.id = i.ticket)
          LEFT JOIN puretickets_context as c
