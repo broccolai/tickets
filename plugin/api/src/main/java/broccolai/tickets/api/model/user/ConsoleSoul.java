3f095458540b5bc18a36fe7c9beb541b6e8db24b
broccolai <me@broccol.ai>
2021-06-28 11:40:17 +0100

    Implement Soul.Abstract#equals
    Also makes all subclasses extend Soul.Abstract

--- plugin/api/src/main/java/broccolai/tickets/api/model/user/ConsoleSoul.java
+++ plugin/api/src/main/java/broccolai/tickets/api/model/user/ConsoleSoul.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 import net.kyori.adventure.audience.Audience;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-public abstract class ConsoleSoul implements OnlineSoul {
+public abstract class ConsoleSoul extends Soul.Abstract implements OnlineSoul {
 
     public static final @NonNull UUID UNIQUE_ID = new UUID(0, 0);
 
--- plugin/api/src/main/java/broccolai/tickets/api/model/user/Soul.java
+++ plugin/api/src/main/java/broccolai/tickets/api/model/user/Soul.java
@@ -2,6 +2,7 @@ package broccolai.tickets.api.model.user;
 
 import java.util.UUID;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.dataflow.qual.Pure;
 
 public interface Soul {
@@ -12,4 +13,21 @@ public interface Soul {
     @Pure
     @NonNull String username();
 
+    abstract class Abstract implements Soul {
+
+        @Override
+        public boolean equals(final @Nullable Object obj) {
+            if (!(obj instanceof Soul otherSoul)) {
+                return false;
+            }
+
+            return this.uuid().equals(otherSoul.uuid());
+        }
+
+        @Override
+        public int hashCode() {
+            return this.uuid().hashCode();
+        }
+
+    }
 }
