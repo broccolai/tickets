5333545aadbb61ad0d3053d998a07ca9fae3157a
broccolai <me@broccol.ai>
2021-07-24 17:45:15 +0100

    fix: Move to Configurate Visitor
    Allows for more than one level of nested entries
    
    Co-authored-by: MD <1917406+mdcfe@users.noreply.github.com>

--- plugin/core/src/main/java/broccolai/tickets/core/configuration/NewLocaleConfiguration.java
+++ plugin/core/src/main/java/broccolai/tickets/core/configuration/NewLocaleConfiguration.java
@@ -10,6 +10,8 @@ import java.util.Map;
 import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.ConfigurateException;
 import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.ConfigurationVisitor;
+import org.spongepowered.configurate.NodePath;
 import org.spongepowered.configurate.yaml.NodeStyle;
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 
@@ -35,28 +37,11 @@ public final class NewLocaleConfiguration {
         node = node.mergeFrom(this.loadDefault(configuration.locale));
 
         loader.save(node);
-        this.entries = this.load(node);
+        this.entries = node.visit(new LocaleVisitor());
     }
 
     public String get(final String key) {
         return this.entries.get(key);
-    }
-
-    private Map<String, String> load(final ConfigurationNode node) {
-        Map<String, String> results = new HashMap<>();
-
-        for (final ConfigurationNode value : node.childrenMap().values()) {
-            if (value.isMap()) {
-                for (final ConfigurationNode childrenValue : value.childrenMap().values()) {
-                    results.put(value.key() + "." + childrenValue.key(), childrenValue.getString());
-                }
-                continue;
-            }
-
-            results.put((String) value.key(), value.getString());
-        }
-
-        return results;
     }
 
     //todo: NEEDS TO COPY ALL LOCALES REGARDLESS
@@ -71,6 +56,67 @@ public final class NewLocaleConfiguration {
 
     private String makeLocaleFileName(final String locale) {
         return "locale_" + locale + ".yml";
+    }
+
+    private static final class LocaleVisitor implements ConfigurationVisitor.Safe<Map<String, String>, Map<String, String>> {
+
+        @Override
+        public Map<String, String> newState() {
+            return new HashMap<>();
+        }
+
+        @Override
+        public void beginVisit(final ConfigurationNode node, final Map<String, String> state) {
+            // no-op
+        }
+
+        @Override
+        public void enterNode(final ConfigurationNode node, final Map<String, String> state) {
+            // no-op
+        }
+
+        @Override
+        public void enterMappingNode(final ConfigurationNode node, final Map<String, String> state) {
+            // no-op
+        }
+
+        @Override
+        public void enterListNode(final ConfigurationNode node, final Map<String, String> state) {
+            // no-op
+        }
+
+        @Override
+        public void enterScalarNode(final ConfigurationNode node, final Map<String, String> state) {
+            state.put(this.path(node), node.getString());
+        }
+
+        @Override
+        public void exitMappingNode(final ConfigurationNode node, final Map<String, String> state) {
+            // no-op
+        }
+
+        @Override
+        public void exitListNode(final ConfigurationNode node, final Map<String, String> state) {
+            // no-op
+        }
+
+        @Override
+        public Map<String, String> endVisit(final Map<String, String> state) {
+            return state;
+        }
+
+        private String path(final ConfigurationNode node) {
+            NodePath path = node.path();
+            if (path.size() == 0) {
+                return "";
+            }
+
+            StringBuilder sb = new StringBuilder(path.get(0).toString());
+            for (int i = 1; i < path.size(); i++) {
+                sb.append(".").append(path.get(i));
+            }
+            return sb.toString();
+        }
     }
 
 }
