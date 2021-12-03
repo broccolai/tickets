a00694bd6e5250e0fa3668596f9009fc440f3df7
broccolai <me@broccol.ai>
2021-12-03 02:55:40 +0000

    rewrite: make ticket a class

--- api/src/main/java/love/broccolai/tickets/api/model/Ticket.java
+++ api/src/main/java/love/broccolai/tickets/api/model/Ticket.java
@@ -1,24 +1,93 @@
 package love.broccolai.tickets.api.model;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import love.broccolai.tickets.api.model.action.Action;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.common.returnsreceiver.qual.This;
 
-public record Ticket(
-        int id,
-        @NonNull UUID creator,
-        @NonNull Instant creationDate,
-        @NonNull String message,
-        @NonNull Set<Action> actions
-) {
+public final class Ticket {
 
-    public @NonNull Ticket withMessage(final @NonNull String message) {
-        return new Ticket(this.id, this.creator, this.creationDate, message, this.actions);
+    private final int id;
+    private final @NonNull UUID creator;
+    private final @NonNull Instant creationDate;
+    private @Nullable UUID assignee;
+    private @NonNull String message;
+    private final @NonNull Set<Action> actions;
+
+    public Ticket(
+            final int id,
+            final @NonNull UUID creator,
+            final @NonNull Instant creationDate,
+            final @Nullable UUID assignee,
+            final @NonNull String message,
+            final @NonNull Set<Action> actions
+    ) {
+        this.id = id;
+        this.creator = creator;
+        this.creationDate = creationDate.truncatedTo(ChronoUnit.SECONDS);
+        this.assignee = assignee;
+        this.message = message;
+        this.actions = actions;
+    }
+
+    public int id() {
+        return this.id;
+    }
+
+    public @NonNull UUID creator() {
+        return this.creator;
+    }
+
+    public @NonNull Instant creationDate() {
+        return this.creationDate;
+    }
+
+    public @NonNull Optional<@NonNull UUID> assignee() {
+        return Optional.ofNullable(this.assignee);
+    }
+
+    public void assignee(final @NonNull UUID assignee) {
+        this.assignee = assignee;
+    }
+
+    public @NonNull String message() {
+        return this.message;
+    }
+
+    public void message(final @NonNull String message) {
+        this.message = message;
+    }
+
+    public @NonNull Set<Action> actions() {
+        return this.actions;
+    }
+
+    @Override
+    public boolean equals(final @Nullable Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Ticket that)) {
+            return false;
+        }
+
+        return this.id == that.id
+                && Objects.equals(this.creator, that.creator)
+                && Objects.equals(this.creationDate, that.creationDate)
+                && Objects.equals(this.message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.creator, this.creationDate, this.message);
     }
 
     public static final class Builder {
@@ -26,6 +95,7 @@ public record Ticket(
         private final int id;
         private final UUID creator;
         private final Instant creationDate;
+        private final UUID assignee;
         private final String message;
         private final Set<Action> actions;
 
@@ -33,11 +103,13 @@ public record Ticket(
                 final int id,
                 final @NonNull UUID creator,
                 final @NonNull Instant creationDate,
+                final @NonNull UUID assignee,
                 final @NonNull String message
         ) {
             this.id = id;
             this.creator = creator;
             this.creationDate = creationDate;
+            this.assignee = assignee;
             this.message = message;
             this.actions = new HashSet<>();
         }
@@ -52,6 +124,7 @@ public record Ticket(
                     this.id,
                     this.creator,
                     this.creationDate,
+                    this.assignee,
                     this.message,
                     Collections.unmodifiableSet(this.actions)
             );
