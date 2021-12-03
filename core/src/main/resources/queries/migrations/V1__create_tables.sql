454cdbae2ec7aee36afb608c0668914345cb7968
broccolai <me@broccol.ai>
2021-12-03 02:57:38 +0000

    rewrite: add action database table

--- core/src/main/resources/queries/migrations/V1__create_tables.sql
+++ core/src/main/resources/queries/migrations/V1__create_tables.sql
@@ -3,5 +3,14 @@ CREATE TABLE tickets_ticket
     `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `creator` varchar(36) NOT NULL,
     `creationDate` timestamp,
+    `assignee` varchar(36),
     `message` varchar(1024)
+);
+
+CREATE TABLE tickets_action
+(
+    `ticket` int NOT NULL AUTO_INCREMENT,
+    `creator` varchar(36) NOT NULL,
+    `creationDate` timestamp NOT NULL,
+    `message` varchar(1024) NOT NULL
 );
