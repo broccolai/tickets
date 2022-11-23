90f30e7cb95f51fd8cf4e048a04ab0bb861a4bc2
broccolai <me@broccol.ai>
2022-11-23 03:53:12 +0000

    fix: insert assignee column for actions, don't select it

--- core/src/main/resources/queries/insert-action.sql
+++ core/src/main/resources/queries/insert-action.sql
@@ -1,3 +1,2 @@
-INSERT INTO tickets_action(`type`, `ticket`, `creator`, `date`, `message`)
-VALUES (:type, :ticket, :creator, :date, :message);
-SELECT LAST_INSERT_ID();
+INSERT INTO tickets_action(`type`, `ticket`, `creator`, `date`, `message`, `assignee`)
+VALUES (:type, :ticket, :creator, :date, :message, :assignee);
