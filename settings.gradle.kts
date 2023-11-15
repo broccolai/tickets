a3a748ae5809e88ae813e226979f7e12a51098a1
broccolai <me@broccol.ai>
2023-11-15 21:55:28 +0000

    build: prepare for minecraft modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,20 @@
-rootProject.name = "tickets"
-
-include("api", "common")
-
-project(":api").name = "tickets-api"
-project(":common").name = "tickets-common"
-
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+setupSubproject(name = "api")
+setupSubproject(name = "common")
+
+setupSubproject(folder = "minecraft", name = "common")
+
+fun setupSubproject(folder: String? = null, name: String) {
+    val formattedName = listOfNotNull("tickets", folder, name)
+        .joinToString("-")
+
+    include(name)
+
+    val project = project(":$name")
+    project.name = formattedName
+
+    folder?.let {
+        project.projectDir = file("$folder/$name")
+    }
+}
